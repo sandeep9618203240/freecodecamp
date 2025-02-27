38c562f4a75aced94452aa3d1f0bd1ab923811bf
sandeepkolli09 <sandeepkolli961@gmail.com>
2025-02-27 10:55:03 +0530

    funtion creatd

--- script.py
+++ script.py
@@ -1,4 +1,15 @@
-def calculate():
-    return 2 + 2
+import numpy as np
 
+def calculate(numbers):
+    mat = np.array(numbers).reshape(3, 3)
+    ans={
+        'mean': [np.mean(mat, axis=0).tolist(), np.mean(mat, axis=1).tolist(), np.mean(mat)],
+        'variance': [np.var(mat, axis=0).tolist(), np.var(mat, axis=1).tolist(), np.var(mat)],
+        'standard deviation': [np.std(mat, axis=0).tolist(), np.std(mat, axis=1).tolist(), np.std(mat)],
+        'max': [np.max(mat, axis=0).tolist(), np.max(mat, axis=1).tolist(), np.max(mat)],
+        'min': [np.min(mat, axis=0).tolist(), np.min(mat, axis=1).tolist(), np.min(mat)],
+        'sum': [np.sum(mat, axis=0).tolist(), np.sum(mat, axis=1).tolist(), np.sum(mat)]
+    }
 
+res = calculate([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(res)
